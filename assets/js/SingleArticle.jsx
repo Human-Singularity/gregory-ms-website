efc8341ab30af79aac021318d3534998ca7dcaf4
Bruno Amaral <mail@brunoamaral.eu>
2024-05-30 22:48:31 +0100

    adjust to fit the new api structure

--- assets/js/SingleArticle.jsx
+++ assets/js/SingleArticle.jsx
@@ -39,27 +39,43 @@ export function SingleArticle() {
     }
 
     return (
-		<>
-			<span id="article" className="anchor"></span>
-			<p><strong className='text-muted'>ID</strong>: <span id="id" data-datetime={article.article_id}>{article.article_id}</span></p>
-			<p><strong className='text-muted'>Short Link: </strong> <a href={`https://gregory-ms.com/articles/${article.article_id}`}>https://gregory-ms.com/articles/{article.article_id}</a> </p>
-			<p><strong className='text-muted'>Discovery Date</strong>: <span id="discovery_date" data-datetime={article.discovery_date}>{article.discovery_date ? formatDate(article.discovery_date) : 'Unknown'}</span></p>
-			<p><strong className='text-muted'>Published Date</strong>: <span id="published_date" data-datetime={article.published_date}>{article.published_date ? formatDate(article.published_date) : 'Unknown'}</span></p>
-			<p><strong className='text-muted'>Source</strong>: <span id="source">{article.publisher}</span></p>
-			<p><strong className='text-muted'>Link</strong>: <span id="link"><a href={article.link}>{article.link}</a></span></p>
-			<p><strong className='text-muted'>Manual Selection</strong>: <span id="relevant">{article.relevant === null ? "not set" : article.relevant.toString()}</span></p>
-			<p><strong className='text-muted'>Machine Learning Prediction (Gaussian Naive Bayes Model)</strong>: <span id="ml_prediction_gnb">{article.ml_prediction_gnb === null ? "not set" : article.ml_prediction_gnb.toString()}</span></p>
+        <>
+            <span id="article" className="anchor"></span>
+            <p><strong className='text-muted'>ID</strong>: <span id="id" data-datetime={article.article_id}>{article.article_id}</span></p>
+            <p><strong className='text-muted'>Short Link: </strong> <a href={`https://gregory-ms.com/articles/${article.article_id}`}>https://gregory-ms.com/articles/{article.article_id}</a> </p>
+            <p><strong className='text-muted'>Discovery Date</strong>: <span id="discovery_date" data-datetime={article.discovery_date}>{article.discovery_date ? formatDate(article.discovery_date) : 'Unknown'}</span></p>
+            <p><strong className='text-muted'>Published Date</strong>: <span id="published_date" data-datetime={article.published_date}>{article.published_date ? formatDate(article.published_date) : 'Unknown'}</span></p>
+            <p><strong className='text-muted'>Publisher</strong>: <span id="publisher">{article.publisher}</span></p>
+            <p><strong className='text-muted'>Link</strong>: <span id="link"><a href={article.link}>{article.link}</a></span></p>
+            <p><strong className='text-muted'>Manual Selection</strong>: <span id="relevant">{article.relevant === null ? "not set" : article.relevant.toString()}</span></p>
 
-			<div className="post-text" id="takeaways">
-				<h2>Main Takeaways</h2>
-				<p>{article.takeaways}</p>
-			</div>
+            {article.ml_predictions.length > 0 && (
+                <>
+                    <p><strong className='text-muted'>Machine Learning Prediction (Gaussian Naive Bayes Model)</strong>: <span id="ml_prediction_gnb">{article.ml_predictions[0].gnb === null ? "not set" : article.ml_predictions[0].gnb.toString()}</span></p>
+                </>
+            )}
 
-			<div className="post-text" id="abstract">
-				<h2>Abstract</h2>
-				<div dangerouslySetInnerHTML={{ __html: article.summary }}></div>
-			</div>
-		</>
-	);
+            <div className="post-text" id="takeaways">
+                <h2>Main Takeaways</h2>
+                <p>{article.takeaways}</p>
+            </div>
 
+            <div className="post-text" id="abstract">
+                <h2>Abstract</h2>
+                <div dangerouslySetInnerHTML={{ __html: article.summary }}></div>
+            </div>
+
+            <div className="authors-list">
+                <h2>Authors</h2>
+                <ul>
+                    {article.authors.map(author => (
+                        <li key={author.author_id}>
+                            {author.given_name} {author.family_name} {author.ORCID && <a href={author.ORCID}>(ORCID)</a>}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+        </>
+    );
 }
