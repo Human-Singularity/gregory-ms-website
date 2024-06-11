bb326ee0e3c4e5695b89c17210cb161ad4bf6a4d
Bruno Amaral <mail@brunoamaral.eu>
2024-06-11 12:28:30 +0100

    avoid creating route more than once

--- assets/js/gregory_api_all_articles.jsx
+++ assets/js/gregory_api_all_articles.jsx
@@ -9,15 +9,23 @@ function App() {
 		return (
 			<Router>
 				<Routes>
-					<Route path="/articles/" element={<ArticleList apiEndpoint="https://api.gregory-ms.com/teams/1/articles" page_path='/articles' />} />
-					<Route path="/articles/page/:pageNumber" element={<ArticleList apiEndpoint="https://api.gregory-ms.com/teams/1/articles" page_path='/articles' />} />
 					<Route path="/articles/:articleId/:articleSlug" element={<SingleArticle />} />
 					<Route path="/articles/:articleId" element={<SingleArticle />} />
 					<Route path="/articles/author/:authorId" element={AuthorProfile} />
+					<Route path="/articles/" element={<ArticleList apiEndpoint="https://api.gregory-ms.com/teams/1/articles" page_path='/articles' />} />
+					<Route path="/articles/page/:pageNumber" element={<ArticleList apiEndpoint="https://api.gregory-ms.com/teams/1/articles" page_path='/articles' />} />
 				</Routes>
 			</Router>
 		);
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// Create a root if it doesn't exist yet
+const rootElement = document.getElementById("root");
+if (!rootElement._reactRootContainer) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<App />);
+} else {
+    // If the root already exists, just render the App
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<App />);
+}
