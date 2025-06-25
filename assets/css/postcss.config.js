3a5079ce2767f299c42496dab6c96f68c6852d66
Bruno Amaral <mail@brunoamaral.eu>
2025-06-25 14:49:57 +0100

    remove dead code

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,44 +1,6 @@
 // PostCSS with PurgeCSS disabled for debugging
 module.exports = {
     plugins: {
-        // PurgeCSS completely disabled for debugging
-        /*
-        // '@fullhuman/postcss-purgecss': {
-//             content: ['./layouts/ /*.html', './themes/NowUI-Pro/layouts/ /*.html', './content/ /*.html', './content/ /*.md', './assets/js/ /*.jsx'],
-//             safelist: {
-//               standard: [
-//                 /nav-open/,
-//                 /badge/,
-//                 /badge-/,
-//                 /relevance-/,
-//                 /algorithm-/,
-//                 /ml-prediction/,
-//                 /badge-high-relevance/,
-//                 /badge-medium-relevance/, 
-//                 /badge-low-relevance/,
-//                 /algorithm-pubmed-bert/,
-//                 /algorithm-lgbm-tfidf/,
-//                 /algorithm-lstm/
-//               ],
-//               greedy: [
-//                 /.animate.*/,
-//                 /.badge.*/,
-//                 /.ml-prediction.*/,
-//                 /.relevance.*/,
-//                 /.algorithm.*/,
-//                 /.ml-.*/,
-//               ]
-//             },
-//             fontFace: false,
-//             variables: false
-//         },
-//         */
-//         autoprefixer: {},
-//         cssnano: {
-//             preset: [
-//                 'default',
-//                 { 'discardComments': { 'removeAll': true } }
-//             ]
-//         }
+
     }
 };
