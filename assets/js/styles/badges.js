e79efbed1ccf955b42cf03177c368b3832473efb
Bruno Amaral <mail@brunoamaral.eu>
2025-06-25 16:56:53 +0100

    changes to badges

---
+++ assets/js/styles/badges.js
@@ -0,0 +1,7 @@
+/**
+ * Badge styles for React components
+ * This file ensures that badge styles are consistent between Hugo and React components
+ */
+
+/* Import necessary styles */
+@import "../scss/badges.scss";
